139f1e9933cb087e76d0a30ce058ce1ac759ba0c
aflazur <mdaflazurrahaman2022@gmail.com>
2026-03-26 00:19:56 +0600

    Solve Navbar

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
 
 import Footer from "../Components/Footer";
-import Navbar from "../components/Navbar";
+import Navbar from "../Components/Navbar";
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-[#F8F7FF]">
-      <Navbar />
+      <Navbar/>
       <main className="flex-1">
         <Outlet />
       </main>
